c69f53277b2cca7f0e49b9f9ac2e54a53ab1a41d
TheCarosatzx <108580335+TheCarosatzx@users.noreply.github.com>
2022-11-25 10:33:10 +0700

    Add files via upload

--- Carosatzx.js
+++ Carosatzx.js
@@ -274,6 +274,66 @@ cann.sendMessage(`${num}@s.whatsapp.net`, { text: 'TheCarosatzx', templateButton
 }
 }
 break
+
+
+case 'nowa': {
+if (!args[0]) return reply(`Kirim perintah ${prefix+command} <nomer>`)
+var noteks = args[0]
+if (!noteks.includes('x')) return reply('lah?')
+reply(mess.wait)
+function countInstances(string, word) {
+return string.split(word).length - 1;
+}
+var nomer0 = noteks.split('x')[0]
+var nomer1 = noteks.split('x')[countInstances(noteks, 'x')] ? noteks.split('x')[countInstances(noteks, 'x')] : ''
+var random_length = countInstances(noteks, 'x')
+var random;
+if (random_length == 1) {
+random = 10
+} else if (random_length == 2) {
+random = 100
+} else if (random_length == 3) {
+random = 1000
+}
+var nomerny = `LIST NOMER WHATSAPP\n\nPunya Bio/status/info\n`
+var no_bio = `\nTanpa Bio/status/info || \nHey there! I am using WhatsApp.\n`
+var no_watsap = `\nTidak Terdaftar\n`
+for (let i = 0; i < random; i++) {
+var nu = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+var dom1 = nu[Math.floor(Math.random() * nu.length)]
+var dom2 = nu[Math.floor(Math.random() * nu.length)]
+var dom3 = nu[Math.floor(Math.random() * nu.length)]
+var dom4 = nu[Math.floor(Math.random() * nu.length)]
+var rndm;
+if (random_length == 1) {
+rndm = `${dom1}`
+} else if (random_length == 2) {
+rndm = `${dom1}${dom2}`
+} else if (random_length == 3) {
+rndm = `${dom1}${dom2}${dom3}`
+} else if (random_length == 4) {
+rndm = `${dom1}${dom2}${dom3}${dom4}`
+}
+var anu = await cann.onWhatsApp(`${nomer0}${i}${nomer1}@s.whatsapp.net`);
+var anuu = anu.length !== 0 ? anu : false
+try {
+try {
+var anu1 = await cann.fetchStatus(anu[0].jid)
+} catch {
+var anu1 = '401'
+}
+if (anu1 == '401' || anu1.status.length == 0) {
+no_bio += `wa.me/${anu[0].jid.split("@")[0]}\n`
+} else {
+nomerny += `wa.me/${anu[0].jid.split("@")[0]}\nBiography : ${anu1.status}\nDate : ${moment(anu1.setAt).tz('Asia/Jakarta').format('HH:mm:ss DD/MM/YYYY')}\n\n`
+}
+} catch {
+no_watsap += `${nomer0}${i}${nomer1}\n`
+}
+}
+reply(`${nomerny}${no_bio}${no_watsap}`)
+}
+break
 /============================================/
 case 'santetstick': {
 if (args.length < 1) return m.reply(`*Syntax Error!*\n\nUse : ${command} nomor target|amount spam|timer\nExample : ${command} 62888xxxx|1|10s\n\n\ns = Second/Detik\n\n`)
